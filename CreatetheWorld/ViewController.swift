e2a156bc671bf1c405f5f89c513aff7fa3cd0339
playin10is <playin10is@gmail.com>
2016-10-23 20:57:10 -0700

    hello world

---
+++ CreatetheWorld/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  CreatetheWorld
+//
+//  Created by saeheng on 10/20/16.
+//  Copyright © 2016 Saeheng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        print ("hello world")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
